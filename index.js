74964de04def3119c1e28fb50e7b3bbe74d96989
jnwozo <jnwozo@outlook.com>
2023-11-25 11:25:36 -0500

    Intial commit: Hello,git and codedeploy!

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const server = http.createServer((req,
+res) => {
+	res.writeHead(200, { 'Content-Type' :
+'text/plain' });
+	res.end('Hello, Git and CodeDeploy!
+\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+	console.log('server running at
+http://localhost:${PORT}/';
+}); 
